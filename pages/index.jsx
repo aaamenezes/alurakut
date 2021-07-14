3e5f887bf35b554a2f5e3f3d61c12815f59b8f4a
andre-menezes <andre.menezes@vnda.com.br>
2021-07-13 22:17:30 -0300

    Criar estados para o formulário e adicionar comunidades

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,22 +1,38 @@
 import { Box } from '../src/components/Box'
 import { ProfileRelationsBoxWrapper } from '../src/components/ProfileRelations'
 import MainGrid from '../src/components/MainGrid'
-import { AlurakutMenu, OrkutNostalgicIconSet } from '../src/lib/AlurakutCommons'
+import {
+  AlurakutMenu, AlurakutProfileSidebarMenuDefault, OrkutNostalgicIconSet
+} from '../src/lib/AlurakutCommons'
+import { useState } from 'react'
 
 function ProfileSidebar({ githubUser }) {
   const imageURL = `https://github.com/${githubUser}.png`
   return (
-    <Box>
+    <Box as='aside'>
       <img
         src={imageURL}
         alt="profile picture"
         style={{ borderRadius: '8px'}}
       />
+      <hr />
+      <p>
+        <a className='boxLink' href={`https://github.com/${githubUser}`}>
+          @{githubUser}
+        </a>
+      </p>
+      <hr />
+      <AlurakutProfileSidebarMenuDefault />
     </Box>
   )
 }
 
 export default function Home() {
+  const initialComunity = {
+    title: 'Eu odeio acorder cedo',
+    image: 'https://alurakut.vercel.app/capa-comunidade-01.jpg'
+  }
+  const [ comunities, setComunities ] = useState([initialComunity])
   const githubUser = 'aaamenezes'
   const favoritePeople = [
     'juunegreiros',
@@ -35,7 +51,7 @@ export default function Home() {
   
   return (
     <>
-      <AlurakutMenu />
+      <AlurakutMenu githubUser={githubUser} />
       <MainGrid>
         <div className='profileArea' style={{ gridArea: 'profile' }}>
           <ProfileSidebar githubUser={githubUser} />
@@ -45,8 +61,57 @@ export default function Home() {
             <h1 className="title">Bem vinde</h1>
             <OrkutNostalgicIconSet />
           </Box>
+          <Box>
+            <h2 className='subTitle'>O que vc deseja fazer?</h2>
+            <form onSubmit={event => {
+              event.preventDefault()
+              const formData = new FormData(event.target)
+              const addedComunnity = {
+                title: formData.get('title'),
+                image: formData.get('image')
+              }
+              setComunities([ ...comunities, addedComunnity ])
+            }}>
+              <div>
+                <input
+                  type='text'
+                  name='title'
+                  placeholder='Nome da comunidade...'
+                  aria-label='Nome da comunidade...'
+                />
+              </div>
+              <div>
+                <input
+                  type='text'
+                  name='image'
+                  placeholder='URL de capa'
+                  aria-label='URL de capa'
+                />
+              </div>
+              <div>
+                <button type="submit">Criar comunidade</button>
+              </div>
+            </form>
+          </Box>
         </div>
         <div className='profileRelationsArea' style={{ gridArea: 'profileRelations' }}>
+          <ProfileRelationsBoxWrapper>
+            <h2 className='smallTitle'>
+              Comunidades ({comunities.length})
+            </h2>
+            <ul>
+              {comunities.map(comunity => {
+                return (
+                  <li key={comunity.title}>
+                    <a href={`https://github.com/${comunity.title}`}>
+                      <img src={comunity.image} />
+                      <span>{comunity.title}</span>
+                    </a>
+                  </li>
+                )
+              })}
+            </ul>
+          </ProfileRelationsBoxWrapper>
           <ProfileRelationsBoxWrapper>
             <h2 className='smallTitle'>
               Pessoas da comunidade ({favoritePeople.length})
@@ -54,8 +119,8 @@ export default function Home() {
             <ul>
               {favoritePeople.map(person => {
                 return (
-                  <li>
-                    <a href={`https://github.com/${person}`} key={person}>
+                  <li key={person}>
+                    <a href={`https://github.com/${person}`}>
                       <img src={`https://github.com/${person}.png`} />
                       <span>{person}</span>
                     </a>
